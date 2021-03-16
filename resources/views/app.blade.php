3b887aaf0d3a6b04bb337c077edcb13ef32a46a8
Salzer242 <wonderbakaledavid@gmail.com>
2021-03-16 18:58:54 +0100

    Add route names and use them in views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
        <footer>
             <p>
                 &copy; copyright {{ date('Y') }} 
-                &middot; <a href="/about-us">About Us</a>
+                &middot; <a href="{{ route('about') }}">About Us</a>
             </p>
        </footer>
     </body>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,5 +6,5 @@
     <title>Abous Us | Hello City</title>
     <p>Build with &hearts; by LES TEACHERS DU NET.</p>
 
-    <p><a href="/">Revenir à la page d'accueil</a></p>
+    <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
 @endsection
