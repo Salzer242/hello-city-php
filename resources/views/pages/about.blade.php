9129b114855aba028650218b4e35369548bbe76c
Salzer242 <wonderbakaledavid@gmail.com>
2021-03-16 20:58:08 +0100

    Add styling with Tailwind CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,12 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content') 
-    <img src="{{ asset('images/audi.jpg') }}" alt="logo">
-    <p>Build with &hearts; by LES TEACHERS DU NET.</p>
+    <img src="{{ asset('images/audi.jpg') }}" alt="logo" class="my-12 rounded shadow-md h-40">
+    <h2 class="mb-5 text-gray-700">
+        Build with <span class="text-pink-500">&hearts;</span> by LES TEACHERS DU NET.
+    </h2>
 
-    <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
+    <p class="">
+        <a href="{{ route('home') }}" class=" text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'accueil</a>
+    </p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,9 @@
 
 
 @section('content')
-       <img src="{{ asset('images/Congo.jpg') }}" alt="Drapeau">
-       <h1>Hello from Senegal !</h1>
+       <img src="{{ asset('images/Congo.jpg') }}" alt="Drapeau" class="mt-12 rounded shadow-md h-40">
 
-       <p>It's currenty {{ date('H:i A') }}</p>
+       <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Senegal !</h1>
+
+       <p class="mt-2 text-lg text-gray-800">It's currenty {{ date('H:i A') }}</p>
 @endsection
