e93cfefa26fbd3456d9725045f84862344281572
Salzer242 <wonderbakaledavid@gmail.com>
2021-03-16 15:33:09 +0100

    Add proper page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,9 @@
 @extends('app')
 
+@section('title', 'About Us | Hello City')
+
 @section('content') 
+    <title>Abous Us | Hello City</title>
     <p>Build with &hearts; by LES TEACHERS DU NET.</p>
 
     <p><a href="/">Revenir à la page d'accueil</a></p>
