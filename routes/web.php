6d0404aaaed1e5570288bb01e0b36786d6890690
Salzer242 <wonderbakaledavid@gmail.com>
2021-03-17 16:26:42 +0100

    Make use of Route::view instead of Route::get

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
+/*Route::get('/', function() {
     return view('pages/home');
-})->name('home');
+})->name('home');*/
 
-Route::get('/about-us', function(){
+/*Route::get('/about-us', function(){
     return view('pages.about');
-})->name('about');
+})->name('about');*/
+
+// Format PHP
+/*
+Route::get('/', fn() => view('pages/home'))->name('home');
+
+Route::get('/about-us', fn() => view('pages/about'))->name('about');
+*/
+
+// Format LARAVEL
+Route::view('/', 'pages/home')->name('home');
+Route::view('/about-us', 'pages/about')->name('about');
